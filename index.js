074b631fdf02d34d04d47704aa6e75123e08af8a
NewDonkCity <50597915+NewDonkCity@users.noreply.github.com>
2020-08-05 18:09:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('Notepad is better than Microsoft Word');
 });
 
 client.login(process.env.BOT_TOKEN);
