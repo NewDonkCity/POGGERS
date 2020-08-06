8203a616731058e9c0ab642897864e8fc5ffc531
NewDonkCity <50597915+NewDonkCity@users.noreply.github.com>
2020-08-05 18:15:11 -0600

    Made it better
    Made it better

--- index.js
+++ index.js
@@ -5,4 +5,26 @@ client.once('ready', () => {
 	console.log('Notepad is better than Microsoft Word');
 });
 
+client.on('message', message => {
+	var message_content = message.content.toLowerCase();
+	if (message_content.includes("word")) {
+		message.channel.send("Notepad is better than Microsoft Word");
+	}
+	if (message_content.includes("notepad")) {
+		message.channel.send("Notepad is better than Microsoft Word");
+	}
+	if (message_content.includes("text")) {
+		message.channel.send("Notepad is better than Microsoft Word");
+	}
+	if (message_content.includes("editor")) {
+		message.channel.send("Notepad is better than Microsoft Word");
+	}
+	if (message_content.includes("visual studio")) {
+		message.channel.send("Notepad is better than Microsoft Word");
+	}
+	if (message_content.includes("code")) {
+		message.channel.send("Notepad is better than Microsoft Word");
+	}
+}
+
 client.login(process.env.BOT_TOKEN);
